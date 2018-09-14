e2e6818b4b45a5b7e5364552c8a0d576aca64238
Akshay Valiya Rakkanchath <akshayrakkanchath@gmail.com>
2018-09-14 20:45:00 +0530

    ADDED proptyes ADDED initial tests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'proptypes';
 
 class Form extends React.Component {
   componentDidMount() {
@@ -10,5 +11,17 @@ class Form extends React.Component {
     );
   }
 }
+
+Form.propTypes = {
+  config: PropTypes.arrayOf(PropTypes.shape({
+    identifier: PropTypes.string.isRequired,
+    order: PropTypes.number,
+    props: PropTypes.object,
+    attrs: PropTypes.arrayOf(PropTypes.object),
+    type: PropTypes.string.isRequired
+  })),
+  onSubmit: PropTypes.func,
+  onChange: PropTypes.func
+};
 
 export default Form;
--- test/index.js
+++ test/index.js
@@ -4,15 +4,70 @@ import { expect } from 'chai';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { spy } from 'sinon';
-import Component from '../src/index';
+import Form from '../src/index';
 
 jsDOM();
 Enzyme.configure({ adapter: new Adapter() });
-spy(Component.prototype, 'componentDidMount');
 
-describe('<Foo />', () => {
-  it('calls componentDidMount', () => {
-    mount(<Component />);
-    expect(Component.prototype.componentDidMount).to.have.property('callCount', 1);
+const options = {
+  config: [{
+    identifier: 'firstName',
+    order: 0,
+    props: {},
+    type: 'input'
+  }, {
+    identifier: 'lastName',
+    order: 1,
+    props: {},
+    type: 'input'
+  }, {
+    identifier: 'gender',
+    order: 2,
+    props: {},
+    type: 'dropdown'
+  }],
+  onSubmit: spy(),
+  onChange: spy()
+};
+
+const typeMappings = {
+  'input': 'inupt',
+  'dropdown': 'select'
+};
+
+spy(Form.prototype, 'componentDidMount');
+
+describe('<Form />', () => {
+  let form;
+
+  before(function() {
+    form = mount(<Form {...options}/>);
+  });
+
+  it('Form componentDidMount', () => {
+    expect(Form.prototype.componentDidMount).to.have.property('callCount', 1);
+  });
+
+  it('Form elements rendered', () => {
+    const inputs = form.find(typeMappings['input']);
+    const dropdowns = form.find(typeMappings['input']);
+
+    expect(inputs).to.have.length(2);
+    expect(dropdowns).to.have.length(1);
+  });
+
+  it('Input elements rendered', () => {
+    const inputs = form.find(typeMappings['input']);
+
+    for (let i = 0, len = inputs.length; i < len; i++) {
+      let input = inputs.at(0),
+        inputConfig = options.config.find((config) => {
+          return config.order === i;
+        });
+
+      for (let key in inputConfig) {
+        expect(input.prop(key)).equals(inputConfig[key]);
+      }
+    }
   });
 });
